2b0b0cd0983eddaad9a976fcaa286a80723a5420
kartik1998 <kartikrawat61@gmail.com>
2022-02-23 00:00:32 +0530

    chore: add http streaming

--- writeable_streams_backpressuring/backpressure.js
+++ writeable_streams_backpressuring/backpressure.js
@@ -1,14 +1,14 @@
 const { createReadStream, createWriteStream } = require("fs");
 const readStream = createReadStream("../anime_dancing.mp4");
 const writeStream = createWriteStream("../copy.mp4", {
-  highWaterMark: 1628 // highWaterMark is in bits, use it to manage your memory wisely
-  // highWaterMark: 162820 
+  highWaterMark: 1628, // highWaterMark is in bits, use it to manage your memory wisely
+  // highWaterMark: 162820
 });
 
 readStream.on("data", (chunk) => {
   const result = writeStream.write(chunk); // result is fales if writeStream has rejected write due to back pressuring
 
-  if (!result) { 
+  if (!result) {
     console.log("backpressure");
     readStream.pause();
   }
