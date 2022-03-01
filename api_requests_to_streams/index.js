144da9d5a9cdcc9b4d0b19344d9be2b4d5b3833f
kartik1998 <kartikrawat61@gmail.com>
2022-03-01 16:18:53 +0530

    chore: add example for converting api requests with skips to readable stream

---
+++ api_requests_to_streams/index.js
@@ -0,0 +1,40 @@
+const axios = require("axios").default;
+const { Readable } = require("stream");
+
+const BASE_URL = "https://jsonplaceholder.typicode.com/";
+
+class StreamFromApiCalls extends Readable {
+  constructor() {
+    super({ objectMode: true }); // encoding: 'UTF-8' => Converts buffer to string
+    this.skip = 1;
+  }
+
+  _read() {
+    const self = this;
+    axios
+      .get(BASE_URL + `posts/${this.skip}`)
+      .then((res) => {
+        self.push(res.data);
+        if(self.skip == 100) self.push(null);
+        self.skip += 1;
+      })
+      .catch((err) => {
+        self.emit("error", err);
+        self.push(null);
+      });
+  }
+}
+
+async function main() {
+  const stream = new StreamFromApiCalls();
+  stream.on("data", (data) => {
+    console.log(data);
+  });
+  stream.on("error", (err) => {
+    console.log("error recieved");
+    console.log(err.response);
+    console.log("error recieved");
+  });
+}
+
+main();
