18912cd3ee7f582a8664b02132e3ae302f764703
kartik1998 <kartikrawat61@gmail.com>
2022-03-06 18:06:51 +0530

    refactor: async_iterator.js

--- api_requests_to_streams/async_iterator.js
+++ api_requests_to_streams/async_iterator.js
@@ -2,13 +2,13 @@ const axios = require("axios").default;
 const BASE_URL = "https://jsonplaceholder.typicode.com/";
 
 async function* generate() {
+  const promises = [];
   for (let skip = 1; skip <= 100; skip++) {
-    try {
-      const res = await axios.get(BASE_URL + `posts/${skip}`);
-      yield res.data;
-    } catch (err) {
-      console.log(err);
-    }
+    promises.push(axios.get(BASE_URL + `posts/${skip}`));
+  }
+  const res = await Promise.all(promises);
+  for (const d of res) {
+    yield d.data;
   }
 }
 
